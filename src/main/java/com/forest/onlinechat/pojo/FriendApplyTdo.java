d9e091550ef289409b6c42b78b2821b73ea6a4ba
天才阿梓 <3065983144@qq.com>
2024-04-15 21:42:23 +0800

    好友Tdo数据传输对象类

---
+++ src/main/java/com/forest/onlinechat/pojo/FriendApplyTdo.java
@@ -0,0 +1,23 @@
+package com.forest.onlinechat.pojo;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import java.sql.Date;
+
+@Data
+public class FriendApplyTdo extends FriendApply {
+    private Integer id;
+    @NotBlank(message = "邮箱不能为空！")
+    private String email;
+    @NotBlank(message = "密码不能为空！")
+    private String password;
+    private String nickname;
+    private String avatar;
+    private String signature;
+    private String region;
+    private String gender;
+    private Date birthday;
+    private Integer friendGroupId;
+    private Boolean noSignIn;
+}
